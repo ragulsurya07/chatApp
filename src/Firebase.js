6118c79b5eab6b39007f5d77dae67f486f23ae21
ragulsurya07 <102974271+ragulsurya07@users.noreply.github.com>
2023-02-21 11:29:50 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -5,14 +5,14 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyCG2TmlBoUbL5sgI5TZ9V_q1LlcPzUlgxM",
+  apiKey: "Enter--your--api--key",
   authDomain: "authentication-sample-project.firebaseapp.com",
   databaseURL: "http://authentication-sample-project.firebaseio.com",
   projectId: "authentication-sample-project",
   storageBucket: "authentication-sample-project.appspot.com",
-  messagingSenderId: "968629488266",
-  appId: "1:968629488266:web:bd2a71ff3a9f8e95a77b73",
-  measurementId: "G-TM35QZ2N2T"
+  messagingSenderId: "enter--id--of--yours",
+  appId: "enter--your--firebase-app--id",
+  measurementId: "enter--your--measurement--id"
 
 };
 
